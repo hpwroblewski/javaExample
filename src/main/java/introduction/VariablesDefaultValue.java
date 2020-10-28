87ce3fc47b4fe7d5a37e707c64945b230a5ec34b
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2020-10-28 23:47:31 +0100

    Add VariablesDefaultValue.java to introduction package

---
+++ src/main/java/introduction/VariablesDefaultValue.java
@@ -0,0 +1,26 @@
+package introduction;
+
+public class VariablesDefaultValue {
+
+    static byte byteVariable;
+    static short shortVariable;
+    static int integerVariable;
+    static long longVariable;
+    static float floatVariable;
+    static double doubleVariable;
+    static boolean booleanVariable;
+    static char charVariable;
+
+    public static void main(String[] args) {
+
+        System.out.println("byteVariable = " + byteVariable);
+        System.out.println("shortVariable = " + shortVariable);
+        System.out.println("integerVariable = " + integerVariable);
+        System.out.println("longVariable = " + longVariable);
+        System.out.println("floatVariable = " + floatVariable);
+        System.out.println("doubleVariable = " + doubleVariable);
+        System.out.println("booleanVariable = " + booleanVariable);
+        System.out.println("charVariable = " + charVariable);
+
+    }
+}
