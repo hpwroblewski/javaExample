93c0872453776571bff189895845c1d3912a031e
Hubert Wroblewski <h.p.wroblewski@gmail.com>
2021-02-20 15:11:18 +0100

    Add @BeforeTest and @AfterTest annotation and configure run tests from maven surfire

--- src/main/java/framework/page/SearchPage.java
+++ src/main/java/framework/page/SearchPage.java
@@ -4,28 +4,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-//14.DODANIE PAGE OBJECT PATERN W KTÓRYM KAŻDA STRONA MA DODANE WSZYSTKIE ELEMENTY STRONY DO AUTOMATYZACJI
+//15.DODANIE PAGE OBJECT PATERN W KTÓRYM KAŻDA STRONA MA DODANE WSZYSTKIE ELEMENTY STRONY DO AUTOMATYZACJI
 public class SearchPage {
 
-//    15.DODANIE DRIVERA
+    //    16.DODANIE DRIVERA
     public WebDriver driver;
 
-    //    16.DODANIE SELEKTORÓW
+    //    17.DODANIE SELEKTORÓW
 //    SELENIUM WYKRYWA TYLKO PIERWSZY ELEMENT O TAKIM SAMYM ID
 //    NIE MOŻNA ODWOŁYWAĆ SIĘ DO KLASY KTÓRA MA SPACJE W SWOJEJ NAZWIE
 //    NIE UŻYWA SIĘ XPATH KTÓRY ZACZNYNA SIĘ OD html/div/....
 //    XPATH SELEKTOR //*[@id="WidgetSearchFieldContainer"]/form/div[1]/div[3]/button
 //    CSS SELEKTOR .css-19u3g69 > button:nth-child(1)
-    By searchInputSelector = By.id("downshift-0-label");
+    By cookieButtonSelector = By.id("onetrust-accept-btn-handler");
+    By searchInputSelector = By.id("downshift-0-input");
     By searchInputCheckboxSelector = By.cssSelector("#downshift-0-menu > li:nth-child(1) > div > span");
     By searchButtonSelector = By.cssSelector("#WidgetSearchFieldContainer > form > div.css-hl1f5l > div.css-19u3g69 > button");
 
-    //    17.DODANIE KONSTRUKTORA KTÓRY PRZENOSI DRIVERA PRZEGLĄDARKI
+    //    18.DODANIE KONSTRUKTORA KTÓRY PRZENOSI DRIVERA PRZEGLĄDARKI
     public SearchPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    //    18.DODANIE METOD DLA PAGE OBJECT PATERN KTÓRE POBIERAJĄ SELEKTORY
+    //    19.DODANIE METOD DLA PAGE OBJECT PATERN KTÓRE POBIERAJĄ SELEKTORY
+    public WebElement getCookieButton() {
+        return driver.findElement(cookieButtonSelector);
+    }
+
     public WebElement getSearchInput() {
         return driver.findElement(searchInputSelector);
     }
--- src/test/java/OtodomTest.java
+++ src/test/java/OtodomTest.java
@@ -2,39 +2,49 @@ import framework.config.Base;
 import framework.page.ListPage;
 import framework.page.SearchPage;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 
 //11.DODANIE EXTENDS BASE DZIEDZICZENIA METODY INITIALIZEDRIVER ŻEBY MOŻNA BYŁO UŻYĆ METODY I ZWRÓCIĆ DRIVERA
 public class OtodomTest extends Base {
 
-//    12.DODANIE ADNOTACJI TEST Z TESTNG ORAZ NAZWY TESTU
-//    23.PODPIĘCIE DANYCH Z DATA PROVIDERA DO WYWOŁANIA WIELOKROTNIE TESTU
-    @Test(dataProvider = "getData")
-    public void basePageNavigation(String destination) throws IOException, InterruptedException {
-
+    //    27.DODANIE TAGU @BeforeTest KTÓRY INICJALIZUJE DRIVERA ORAZ ODPALA OKNO PRZEGLĄDARKI
+//    UWAGA! @BeforeTest WYKONUJE SIĘ TYLKO RAZ DLATEGO NIE DODAJE SIĘ W TEJ SEKCJI WYWOŁANIA URL STRONY DLA TESTÓW WYKONYWANYCH WIELOKROTNIE(Z TAGIEM @DataProvider)
+    @BeforeTest
+    public void initialize() throws IOException {
 //        13.INICJALIZACJA DRIVERA PRZEGLĄDARKI I ODPALENIE PRZEGLĄDARKI
         driver = initializeDriver();
-//        25.DODANIE ODCZYTU URL Z data.properties
-        driver.get(prop.getProperty("urlOtodom"));
-        driver.manage().window().maximize();
 
-//        19.PAGE OBJECT PATTERN PRZENOSI DRIVERA PRZEGLĄDARKI
+//        14.MAKSYMALIZACJA OKNA PRZEGLĄDARKI
+        driver.manage().window().maximize();
+    }
+
+    //    12.DODANIE ADNOTACJI TEST Z TESTNG ORAZ NAZWY TESTU
+//    23.PODPIĘCIE DANYCH Z DATA PROVIDERA DO WYWOŁANIA WIELOKROTNIE TESTU
+    @Test(dataProvider = "getData")
+    public void basePageNavigation(String destination) {
+//        26.DODANIE ODCZYTU URL Z data.properties
+        driver.get(prop.getProperty("urlOtodom"));
+
+//        20.PAGE OBJECT PATTERN PRZENOSI DRIVERA PRZEGLĄDARKI
         SearchPage searchPage = new SearchPage(driver);
+
+//        SPRAWDZENIE CZY SEARCHBOX JEST WYŚWIETLONY ZA POMOCĄ Assert.assertTrue ORAZ isDisplayed()
+        Assert.assertTrue(searchPage.getCookieButton().isDisplayed());
 
 //        SPRAWDZENIE CZY SEARCHBOX JEST WYŚWIETLONY ZA POMOCĄ Assert.assertTrue ORAZ isDisplayed()
         Assert.assertTrue(searchPage.getSearchInput().isDisplayed());
 
 //        ----------KOD TESTU----------
+        searchPage.getCookieButton().click();
         searchPage.getSearchInput().click();
         searchPage.getSearchInput().sendKeys(destination);
         searchPage.getSearchInputCheckbox().click();
         searchPage.getSearchButton().click();
 //        -----------------------------
 
-//        19.PAGE OBJECT PATTERN PRZENOSI DRIVERA PRZEGLĄDARKI
+//        20.PAGE OBJECT PATTERN PRZENOSI DRIVERA PRZEGLĄDARKI
         ListPage listPage = new ListPage(driver);
 
 //        SPRAWDZENIE CZY SEARCHBOX JEST WYŚWIETLONY ZA POMOCĄ Assert.assertTrue ORAZ isDisplayed()
@@ -47,16 +57,13 @@ public class OtodomTest extends Base {
         String text = listPage.getAmountOfRecord().getText();
         System.out.println(text);
 //        -----------------------------
-
-//        20.ZAMKNIĘCIE PRZEGLĄDARKI
-        driver.close();
     }
 
-    //        21.DODANIE DATA PROVIDER ODPOWIEDZIALNEGO ZA ODPALANIE TESTÓW Z RÓŻNYMI PARAMETRAMI
+    //        22.DODANIE DATA PROVIDER ODPOWIEDZIALNEGO ZA ODPALANIE TESTÓW Z RÓŻNYMI PARAMETRAMI
     @DataProvider
     public Object[][] getData() {
 
-//        22.PIERWSZY WIERSZ ODPOWIADA ZA ILOŚĆ POWTARZANYCH TESTÓW [2][] - 0,1 CZYLI 2 TESTY A DRUGI WIERSZ ODPOWIADA ZA ILOŚĆ DANYCH WPROWADZANYCH W TEŚCIE [][1] - 0 CZYLI 1 ZMIENNA
+//        23.PIERWSZY WIERSZ ODPOWIADA ZA ILOŚĆ POWTARZANYCH TESTÓW [2][] - 0,1 CZYLI 2 TESTY A DRUGI WIERSZ ODPOWIADA ZA ILOŚĆ DANYCH WPROWADZANYCH W TEŚCIE [][1] - 0 CZYLI 1 ZMIENNA
 //        ERROR - java.lang.ArrayIndexOutOfBoundException - GDY JEST ZA DUŻA WAROTŚĆ WPISANA W PIERWSZYM INDEXIE [ERROR][] KTÓRY WYCHODZI POZA ZAKRES TABLICY (UWAGA INDEX 1 ODPOWIADA TYLKO 0 A NIE 0,1)
 //        ERROR - The data provider is trying to pass 2 parameters but the method OtodomTest#basePageNavigation takes 1 - GDY JEST ZA DUŻA WAROTŚĆ WPISANA W DRUGIM INDEXIE [][ERROR] I JEST ZA MAŁO PARAMETRÓW W METODZIE basePageNavigation(String destination) POD ZNACZNIKIEM @Test(dataProvider = "getData")
         Object[][] data = new Object[2][1];
@@ -65,4 +72,12 @@ public class OtodomTest extends Base {
 
         return data;
     }
+
+    //    26.DODANIE TAGU @AfterTest KTÓRY ZAMYKA OKNO PRZEGLĄDARKI
+    @AfterTest
+    public void tearDown() {
+//        21.ZAMKNIĘCIE PRZEGLĄDARKI
+        driver.close();
+    }
 }
+
